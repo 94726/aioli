c77a41e89b731dc20720f9567b8830b126d4c5a8
94726 <maik.s.kowol@gmail.com>
2024-01-02 21:12:51 +0100

    chore: fix gh-pages page

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -2,4 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['radix-vue/nuxt', '@nuxtjs/tailwindcss', '../aioli/src/nuxt'],
+  app: {
+    baseURL: '/aioli/',
+  },
 })
